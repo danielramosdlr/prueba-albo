31fd6858b6043d2cd1752cec330eed849fc803ec
Daniel Ramos <daniel@dtiservicios.com>
2023-09-01 18:37:40 -0500

    try eks deploy 7.2

--- cluster-eks.tf
+++ cluster-eks.tf
@@ -54,7 +54,7 @@ module "eks" {
   eks_managed_node_groups = {
     master = {
       create_iam_role          = true
-      iam_role_name            = "${var.project}-eks-rol"
+      iam_role_name            = "${var.project}-eks-master-rol"
       iam_role_use_name_prefix = false
       iam_role_additional_policies = {
         CloudWatchAgentServerPolicy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
@@ -73,23 +73,27 @@ module "eks" {
       }
 
     }
+
+    nodes = {
+      create_iam_role          = true
+      iam_role_name            = "${var.project}-eks-nodes-rol"
+      iam_role_use_name_prefix = false
+      iam_role_additional_policies = {
+        CloudWatchAgentServerPolicy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+        AmazonEKS_CNI_Policy        = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+      }
+
+      min_size     = 2
+      max_size     = 2
+      desired_size = 2
+
+      instance_types = ["t3.micro"]
+      capacity_type  = "ON_DEMAND"
+
+      labels = {
+        Environment = "test"
+      }
+
+    }
   }
 }
-
-#module "eks_managed_node_group" {
-#  source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
-#
-#  name            = "${var.project}-eks-node-group"
-#  cluster_name    = module.eks.cluster_name
-#  cluster_version = "1.27"
-#
-#  subnet_ids = module.vpc.private_subnets
-#
-#  cluster_primary_security_group_id = module.eks.cluster_primary_security_group_id
-#  vpc_security_group_ids            = [module.eks.node_security_group_id]
-#
-#
-#  tags = {
-#    project = var.project
-#  }
-#}
