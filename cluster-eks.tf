e144ac21f0518675f907e200d8567cb1393ddb53
Daniel Ramos <daniel@dtiservicios.com>
2023-09-01 10:05:53 -0500

    try eks deploy

--- cluster-eks.tf
+++ cluster-eks.tf
@@ -4,29 +4,28 @@ module "eks" {
   depends_on = [module.vpc]
 
   cluster_name    = "${var.project}-eks"
-  cluster_version = "1.27"
+  cluster_version = "1.21"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
     master-node = {
+      name           = "master-node"
       min_size       = 1
       max_size       = 1
       desire_size    = 1
       instance_types = ["t2.micro"]
-      capacity_type  = "ON_DEMAND"
     }
 
-    nodes-group = {
+    group-nodes = {
+      name           = "group-nodes"
       min_size       = 2
       max_size       = 2
       desired_size   = 2
       instance_types = ["t2.micro"]
-      capacity_type  = "ON_DEMAND"
     }
   }
 
@@ -42,6 +41,13 @@ module "eks" {
     vpc-cni = {
       most_recent = true
     }
+  }
+
+  create_iam_role          = true
+  iam_role_name            = "${var.project}-eks-rol"
+  iam_role_use_name_prefix = false
+  iam_role_additional_policies = {
+    CloudWatchAgentServerPolicy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
   }
 
   cluster_enabled_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
