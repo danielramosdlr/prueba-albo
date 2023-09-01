3b0d3e83599599aa023d609d72643a693fad3ceb
Daniel Ramos <daniel@dtiservicios.com>
2023-09-01 12:13:50 -0500

    try eks deploy 5

--- cluster-eks.tf
+++ cluster-eks.tf
@@ -1,86 +1,86 @@
-module "eks" {
-  source     = "terraform-aws-modules/eks/aws"
-  version    = "~> 19.0"
-  depends_on = [module.vpc]
+# module "eks" {
+#   source     = "terraform-aws-modules/eks/aws"
+#   version    = "~> 19.0"
+#   depends_on = [module.vpc]
 
-  cluster_name    = "${var.project}-eks"
-  cluster_version = "1.21"
+#   cluster_name    = "${var.project}-eks"
+#   cluster_version = "1.21"
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+#   vpc_id     = module.vpc.vpc_id
+#   subnet_ids = module.vpc.private_subnets
 
-  cluster_endpoint_public_access = true
+#   cluster_endpoint_public_access = true
 
-  manage_aws_auth_configmap = true
-  aws_auth_users = [
-    {
-      userarn  = "arn:aws:iam::361656941569:user/dti-cli",
-      username = "dti-cli",
-      groups = [
-        "system:master"
-      ]
-    }
-  ]
-  aws_auth_roles = [
-    {
-      rolearn = "arn:aws:iam::361656941569:role/DTi.Admin",
-      groups = [
-        "system:master"
-      ]
-    }
-  ]
+#   manage_aws_auth_configmap = true
+#   aws_auth_users = [
+#     {
+#       userarn  = "arn:aws:iam::361656941569:user/dti-cli",
+#       username = "dti-cli",
+#       groups = [
+#         "system:master"
+#       ]
+#     }
+#   ]
+#   aws_auth_roles = [
+#     {
+#       rolearn = "arn:aws:iam::361656941569:role/DTi.Admin",
+#       groups = [
+#         "system:master"
+#       ]
+#     }
+#   ]
 
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
+#   cluster_addons = {
+#     coredns = {
+#       most_recent = true
+#     }
+#     kube-proxy = {
+#       most_recent = true
+#     }
+#     vpc-cni = {
+#       most_recent = true
+#     }
+#   }
 
-  cluster_enabled_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+#   cluster_enabled_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
 
-  tags = {
-    project = var.project
-  }
-}
+#   tags = {
+#     project = var.project
+#   }
+# }
 
-module "eks_managed_node_group" {
-  source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+# module "eks_managed_node_group" {
+#   source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
 
-  name            = "${var.project}-eks-node-group"
-  cluster_name    = "${var.project}-eks"
-  cluster_version = "1.21"
-  ami_id          = "ami-0a78e022c1edff515"
+#   name            = "${var.project}-eks-node-group"
+#   cluster_name    = "${var.project}-eks"
+#   cluster_version = "1.21"
+#   ami_id          = "ami-0a78e022c1edff515"
 
-  subnet_ids = module.vpc.private_subnets
+#   subnet_ids = module.vpc.private_subnets
 
-  cluster_primary_security_group_id = module.eks.cluster_primary_security_group_id
-  vpc_security_group_ids            = [module.eks.node_security_group_id]
+#   cluster_primary_security_group_id = module.eks.cluster_primary_security_group_id
+#   vpc_security_group_ids            = [module.eks.node_security_group_id]
 
-  create_iam_role          = true
-  iam_role_name            = "${var.project}-eks-rol"
-  iam_role_use_name_prefix = false
-  iam_role_additional_policies = {
-    CloudWatchAgentServerPolicy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-  }
+#   create_iam_role          = true
+#   iam_role_name            = "${var.project}-eks-rol"
+#   iam_role_use_name_prefix = false
+#   iam_role_additional_policies = {
+#     CloudWatchAgentServerPolicy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+#   }
 
-  min_size     = 1
-  max_size     = 1
-  desired_size = 1
+#   min_size     = 1
+#   max_size     = 1
+#   desired_size = 1
 
-  instance_types = ["t3.micro"]
-  capacity_type  = "ON_DEMAND"
+#   instance_types = ["t3.micro"]
+#   capacity_type  = "ON_DEMAND"
 
-  labels = {
-    Environment = "test"
-  }
+#   labels = {
+#     Environment = "test"
+#   }
 
-  tags = {
-    project = var.project
-  }
-}
+#   tags = {
+#     project = var.project
+#   }
+# }
--- kubernetes-cluster.tf
+++ kubernetes-cluster.tf
@@ -1,102 +1,102 @@
-# resource "aws_iam_role" "eks-iam-role" {
-#   name = "${var.project}-eks-iam-role"
+resource "aws_iam_role" "eks-iam-role" {
+  name = "${var.project}-eks-iam-role"
 
-#   path = "/"
+  path = "/"
 
-#   assume_role_policy = <<EOF
-# {
-#  "Version": "2012-10-17",
-#  "Statement": [
-#   {
-#    "Effect": "Allow",
-#    "Principal": {
-#     "Service": "eks.amazonaws.com"
-#    },
-#    "Action": "sts:AssumeRole"
-#   }
-#  ]
-# }
-# EOF
-# }
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+  {
+   "Effect": "Allow",
+   "Principal": {
+    "Service": "eks.amazonaws.com"
+   },
+   "Action": "sts:AssumeRole"
+  }
+ ]
+}
+EOF
+}
 
-# resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-#   role       = aws_iam_role.eks-iam-role.name
-# }
-# resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-#   role       = aws_iam_role.eks-iam-role.name
-# }
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-iam-role.name
+}
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks-iam-role.name
+}
 
-# resource "aws_eks_cluster" "cluster-eks" {
-#   name     = "${var.project}-eks"
-#   role_arn = aws_iam_role.eks-iam-role.arn
+resource "aws_eks_cluster" "cluster-eks" {
+  name     = "${var.project}-eks"
+  role_arn = aws_iam_role.eks-iam-role.arn
 
-#   vpc_config {
-#     subnet_ids = module.vpc.private_subnets
-#   }
+  vpc_config {
+    subnet_ids = module.vpc.private_subnets
+  }
 
-#   depends_on = [
-#     aws_iam_role.eks-iam-role,
-#   ]
-# }
+  depends_on = [
+    aws_iam_role.eks-iam-role,
+  ]
+}
 
-# resource "aws_iam_role" "node-rol" {
-#   name = "${var.project}-eks-node-rol"
+resource "aws_iam_role" "node-rol" {
+  name = "${var.project}-eks-node-rol"
 
-#   assume_role_policy = jsonencode({
-#     Statement = [{
-#       Action = "sts:AssumeRole"
-#       Effect = "Allow"
-#       Principal = {
-#         Service = "ec2.amazonaws.com"
-#       }
-#     }]
-#     Version = "2012-10-17"
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-#   role       = aws_iam_role.node-rol.name
-# }
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node-rol.name
+}
 
-# resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-#   role       = aws_iam_role.node-rol.name
-# }
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node-rol.name
+}
 
-# resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
-#   policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
-#   role       = aws_iam_role.node-rol.name
-# }
+resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+  role       = aws_iam_role.node-rol.name
+}
 
-# resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-#   role       = aws_iam_role.node-rol.name
-# }
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node-rol.name
+}
 
-# resource "aws_iam_role_policy_attachment" "CloudWatchAgentServerPolicy" {
-#   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-#   role       = aws_iam_role.node-rol.name
-# }
+resource "aws_iam_role_policy_attachment" "CloudWatchAgentServerPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+  role       = aws_iam_role.node-rol.name
+}
 
-# resource "aws_eks_node_group" "node-group" {
-#   cluster_name    = aws_eks_cluster.cluster-eks.name
-#   node_group_name = "${var.project}-node-group"
-#   node_role_arn   = aws_iam_role.node-rol.arn
-#   subnet_ids      = module.vpc.private_subnets
-#   instance_types  = ["t3.micro"]
+resource "aws_eks_node_group" "node-group" {
+  cluster_name    = aws_eks_cluster.cluster-eks.name
+  node_group_name = "${var.project}-node-group"
+  node_role_arn   = aws_iam_role.node-rol.arn
+  subnet_ids      = module.vpc.private_subnets
+  instance_types  = ["t3.micro"]
 
-#   scaling_config {
-#     desired_size = 1
-#     max_size     = 1
-#     min_size     = 1
-#   }
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
 
-#   depends_on = [
-#     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-#     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-#     aws_iam_role_policy_attachment.CloudWatchAgentServerPolicy
-#   ]
-# }
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.CloudWatchAgentServerPolicy
+  ]
+}
