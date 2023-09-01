6e00b35310232d3b162292236af36e4e4dee5433
Daniel Ramos <daniel@dtiservicios.com>
2023-09-01 15:29:06 -0500

    try eks deploy 7

--- cluster-eks.tf
+++ cluster-eks.tf
@@ -11,6 +11,7 @@ module "eks" {
 
   cluster_endpoint_public_access = true
 
+  manage_aws_auth_configmap = true
   aws_auth_users = [
     {
       userarn  = "arn:aws:iam::361656941569:user/dti-cli",
@@ -46,40 +47,50 @@ module "eks" {
   tags = {
     project = var.project
   }
-}
 
-module "eks_managed_node_group" {
-  source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+  eks_managed_node_group_defaults = {
 
-  name            = "${var.project}-eks-node-group"
-  cluster_name    = "${var.project}-eks"
-  cluster_version = "1.27"
-
-  subnet_ids = module.vpc.private_subnets
-
-  cluster_primary_security_group_id = module.eks.cluster_primary_security_group_id
-  vpc_security_group_ids            = [module.eks.node_security_group_id]
-
-  create_iam_role          = true
-  iam_role_name            = "${var.project}-eks-rol"
-  iam_role_use_name_prefix = false
-  iam_role_additional_policies = {
-    CloudWatchAgentServerPolicy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-    AmazonEKS_CNI_Policy        = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   }
 
-  min_size     = 1
-  max_size     = 1
-  desired_size = 1
+  eks_managed_node_groups = {
+    master = {
+      create_iam_role          = true
+      iam_role_name            = "${var.project}-eks-rol"
+      iam_role_use_name_prefix = false
+      iam_role_additional_policies = {
+        CloudWatchAgentServerPolicy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+        AmazonEKS_CNI_Policy        = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+      }
 
-  instance_types = ["t3.micro"]
-  capacity_type  = "ON_DEMAND"
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
 
-  labels = {
-    Environment = "test"
-  }
+      instance_types = ["t3.micro"]
+      capacity_type  = "ON_DEMAND"
 
-  tags = {
-    project = var.project
+      labels = {
+        Environment = "test"
+      }
+
+    }
   }
 }
+
+#module "eks_managed_node_group" {
+#  source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+#
+#  name            = "${var.project}-eks-node-group"
+#  cluster_name    = module.eks.cluster_name
+#  cluster_version = "1.27"
+#
+#  subnet_ids = module.vpc.private_subnets
+#
+#  cluster_primary_security_group_id = module.eks.cluster_primary_security_group_id
+#  vpc_security_group_ids            = [module.eks.node_security_group_id]
+#
+#
+#  tags = {
+#    project = var.project
+#  }
+#}
