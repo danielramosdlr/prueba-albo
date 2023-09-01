06667ce790eb7a154d983ca0bfd3399f68c68878
Daniel Ramos <daniel@dtiservicios.com>
2023-09-01 01:53:42 -0500

    try fix eks node group 10

--- cluster-eks.tf
+++ cluster-eks.tf
@@ -12,18 +12,19 @@ module "eks" {
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
-  #eks_managed_node_groups = {
-  #  nodes-group = {
-  #    min_capacity     = 1
-  #    max_capacity     = 3
-  #    desired_capacity = 1
-  #    instance_types   = ["t3.small"]
-  #    capacity_type    = "ON_DEMAND"
-  #    update_config = {
-  #      max_unavailable_percentage = 50
-  #    }
-  #  }
-  #}
+  eks_managed_node_groups = {
+    nodes-group = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t3.small"]
+      capacity_type  = "ON_DEMAND"
+      update_config = {
+        max_unavailable_percentage = 50
+      }
+    }
+  }
 
   manage_aws_auth_configmap = true
 
@@ -49,34 +50,34 @@ module "eks" {
   }
 }
 
-module "eks_managed_node_group" {
-  source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
-
-  name            = "${var.project}-eks-mng"
-  cluster_name    = "${var.project}-eks"
-  cluster_version = "1.27"
-
-  subnet_ids = module.vpc.private_subnets
-
-  cluster_primary_security_group_id = module.eks.cluster_primary_security_group_id
-  vpc_security_group_ids            = [module.eks.node_security_group_id]
-
-  min_size     = 1
-  max_size     = 10
-  desired_size = 1
-
-  instance_types = ["t3.small"]
-  capacity_type  = "ON_DEMAND"
-
-  taints = {
-    dedicated = {
-      key    = "dedicated"
-      value  = "gpuGroup"
-      effect = "NO_SCHEDULE"
-    }
-  }
-
-  tags = {
-    project = var.project
-  }
-}
+#module "eks_managed_node_group" {
+#  source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+#
+#  name            = "${var.project}-eks-mng"
+#  cluster_name    = "${var.project}-eks"
+#  cluster_version = "1.27"
+#
+#  subnet_ids = module.vpc.private_subnets
+#
+#  cluster_primary_security_group_id = module.eks.cluster_primary_security_group_id
+#  vpc_security_group_ids            = [module.eks.node_security_group_id]
+#
+#  min_size     = 1
+#  max_size     = 10
+#  desired_size = 1
+#
+#  instance_types = ["t3.small"]
+#  capacity_type  = "ON_DEMAND"
+#
+#  taints = {
+#    dedicated = {
+#      key    = "dedicated"
+#      value  = "gpuGroup"
+#      effect = "NO_SCHEDULE"
+#    }
+#  }
+#
+#  tags = {
+#    project = var.project
+#  }
+#}
